0d6966c939feea7282be05fd00fb8ebdf47be818
rajeeev2001 <rajeeev2001@gmail.com>
2026-07-30 22:04:57 +0530

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,21 +1,25 @@
 // ==========================
 // chart.js
-// TradingView Chart Module
+// TradingView Widget
 // ==========================
 
 function loadTradingViewChart() {
 
-    const chart = document.getElementById("tvchart");
+  const chart = document.getElementById("tvchart");
 
-    if (!chart) return;
+  if (!chart) return;
 
-    chart.innerHTML = `
-        <iframe
-            src="https://s.tradingview.com/widgetembed/?symbol=NSE:NIFTY&interval=5&theme=dark"
-            width="100%"
-            height="450"
-            frameborder="0">
-        </iframe>
-    `;
+  chart.innerHTML = `
+  <iframe
+    src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview&symbol=NSE:NIFTY&interval=5&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=F1F3F6&studies=[]&theme=dark&style=1&timezone=Asia/Kolkata"
+    width="100%"
+    height="500"
+    frameborder="0"
+    allowtransparency="true"
+    scrolling="no">
+  </iframe>
+  `;
 
 }
+
+document.addEventListener("DOMContentLoaded", loadTradingViewChart);
